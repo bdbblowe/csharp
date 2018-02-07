d013f0a63dfefb5fcf3d0d8bd6146c04511f1db8
bdbblowe <bdbblowe@gmail.com>
2018-02-06 18:55:31 -0600

    first commit

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        if (input.Length == 0)
+        {
+            return "";
+        }
+        else
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+
+            string reversedString = new string(charArray);
+            return reversedString;
+        }
+    }
+}
